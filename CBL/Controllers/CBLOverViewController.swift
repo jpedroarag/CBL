de0f11dc51568c9c98f8bc8feb51940c239de23c
Adriel Arruda <adriel.arruda@yahoo.com.br>
2018-08-30 10:28:24 -0300

    Adding controllers to storyboards

---
+++ CBL/Controllers/CBLOverViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CBLOverViewController.swift
+//  CBL
+//
+//  Created by Ada 2018 on 30/08/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class CBLOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+         self.navigationController?.navigationBar.topItem?.title = "CBL"
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
