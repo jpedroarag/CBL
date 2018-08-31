c8c476315fba75f9facd1e51d754ea669143ae42
João Pedro Aragão <jpedroarag@hotmail.com>
2018-08-31 12:02:32 -0300

    Updated CBL Overview Controller

--- CBL/Controllers/CBLOverViewController.swift
+++ CBL/Controllers/CBLOverViewController.swift
@@ -16,18 +16,19 @@ class CBLOverViewController: UIViewController {
     @IBOutlet weak var challengeLabel: UILabel!
     @IBOutlet weak var solutionLabel: UILabel!
     
-    var engage: Engage?
-    var investigate: Investigate?
+    var cbl: CBL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         do {
             let context = try CoreDataManager.shared.getContext()
+            let cblEntity = NSEntityDescription.entity(forEntityName: "CBL", in: context)
             let engageEntity = NSEntityDescription.entity(forEntityName: "Engage", in: context)
             let investigateEntity = NSEntityDescription.entity(forEntityName: "Investigate", in: context)
             
-            engage = Engage(entity: engageEntity!, insertInto: context)
-            investigate = Investigate(entity: investigateEntity!, insertInto: context)
+            cbl = CBL(entity: cblEntity!, insertInto: context)
+            cbl?.engage = Engage(entity: engageEntity!, insertInto: context)
+            cbl?.investigate = Investigate(entity: investigateEntity!, insertInto: context)
             
         } catch let error {
             NSLog(error.localizedDescription)
